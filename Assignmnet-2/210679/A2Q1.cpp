531eb1b2d91e2621301753aa6bf975bd56121c68
Nitishkr2003 <109451764+Nitishkr2003@users.noreply.github.com>
2023-07-15 18:01:15 +0530

    Create A2Q1.cpp

---
+++ Assignmnet-2/210679/A2Q1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    void walk()
+    {
+        cout << "I am walking" << endl;
+    }
+};
+
+class Bird : public Animal
+{
+public:
+    void fly()
+    {
+        cout << "I am flying" << endl;
+    }
+};
+
+int main()
+{
+    Bird bird;
+    bird.walk();
+    bird.fly();
+
+    return 0;
